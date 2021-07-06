ce6c976d7ede5b59e994f838eca59c6296ba66b7
vineet srivastav <vineet9798@gmail.com>
2021-07-07 01:50:29 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  //import images
+  AssetImage circle = AssetImage('images/circle.png');
+  AssetImage lucky = AssetImage('images/rupee.png');
+  AssetImage unlucky = AssetImage('images/sadFace.png');
+
+  //get an array
+
+  late List<String> itemArray;
+  late int luckyNumber;
+
+  // init array  with 25 array
+
+  @override
+  void initState() {
+    super.initState();
+    itemArray = List<String>.generate(25, (index) => "empty");
+    generateRandomNumber();
+  }
+
+  generateRandomNumber() {
+    int random = Random().nextInt(25);
+    setState(() {
+      luckyNumber = random;
+    });
+  }
+
+  //define getimage method
+
+  //play game method
+
+  //show all
+
+  //reset all
+  resetGame() {
+    setState(() {
+      itemArray = List<String>.filled(25, "empty");
+    });
+    generateRandomNumber();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
